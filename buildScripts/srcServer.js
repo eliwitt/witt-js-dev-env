740dba52296d05b8d480ec735054d366c7334772
eliwitt <steve_witt@live.com>
2018-04-16 13:38:42 -0400

    Added call to mock service

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -21,11 +21,18 @@ app.get('/', function(req, res) {
 
 app.get('/users', function(req, res){
   // hard coded for demo, Pretending this hits a real db
-  res.json([
-   {"id": 1,"firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
-   {"id": 2,"firstName":"Tammy", "lastName":"Norton", "email":"tnorton@yahoo.com"},
-   {"id": 3,"firstName":"Tina", "lastName":"Lee", "email":"lee.tina@hotmail.com"}
-  ]);
+  // res.json([
+  //  {"id": 1,"firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+  //  {"id": 2,"firstName":"Tammy", "lastName":"Norton", "email":"tnorton@yahoo.com"},
+  //  {"id": 3,"firstName":"Steve", "lastName":"Witt", "email":"steve.witt@hotmail.com"}
+  // ]);
+  var request = require('request');
+  request('http://localhost:3001/users', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Print the google web page.
+    res.send(body);
+  }
+})
 });
 
   app.listen(port, function(err) {
